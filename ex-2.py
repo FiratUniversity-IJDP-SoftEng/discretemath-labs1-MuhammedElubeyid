8f49cb012bc1a24c6b9a932b19aeb0387e79f89e
MuhammedElubeyid <eprrmkz12345@gmail.com>
2025-11-14 18:37:39 +0300

    Add function to list primes up to a given number

--- ex-2.py
+++ ex-2.py
@@ -1,1 +1,7 @@
-
+def list_primes_upto(N):
+    primes = []
+    for num in range(2, N + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+print(list_primes_upto(50))
